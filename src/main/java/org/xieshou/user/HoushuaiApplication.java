1df41206a94f15ecfdcaa7aa954a2408443e9d3d
housh <houshuai0816@126.com>
2017-06-08 10:37:01 +0800

    Initial commit

---
+++ src/main/java/org/xieshou/user/HoushuaiApplication.java
@@ -0,0 +1,12 @@
+package org.xieshou.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoushuaiApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoushuaiApplication.class, args);
+	}
+}
---
+++ src/main/resources/templates/script/hello.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by housh on 2017/6/8.
+ */
+import {Component} from '@angular/core'
+
+
+@Component({
+    selector :'my-app',
+    template: '<h1>Hello {{name}}</h1>'
+
+})
+
+export class AppComponent {name ='Angular'}
